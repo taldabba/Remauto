d29c7b6fb88cf60b5730477c502885fc2f800708
Taim Al-Dabbagh <79340829+taldabba@users.noreply.github.com>
2022-09-24 17:28:40 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from pyPS4Controller.controller import Controller
 
+"""
+JOYSTICK RANGE: -32766 --> 32767
+"""
 
 class MyController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+    def on_R3_right(self, value):
+        print(super().on_R3_right(value))
+        return super().on_R3_right(value)
+
     def on_x_release(self):
        print("BRAKE")
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
-# controller.listen(timeout=60)
+controller.listen(timeout=60)
