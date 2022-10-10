7ca7042ebbfccc6fe4f67dc767acde5ee8df4b68
Taim <taldabba@gmail.com>
2022-10-09 21:22:34 -0400

    Defined PS4 control function stubs

--- test.py
+++ test.py
@@ -12,34 +12,36 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    def on_x_press(self):
-        print("on_x_press")
+    # def on_x_press(self):
+    #     print("on_x_press")
+    # def on_x_release(self):
+    #     print("on_x_release")
 
-    def on_x_release(self):
-        print("on_x_release")
+    # def on_triangle_press(self):
+    #     print("on_triangle_press")
+    # def on_triangle_release(self):
+    #     print("on_triangle_release")
 
-    def on_triangle_press(self):
-        print("on_triangle_press")
+    # def on_circle_press(self):
+    #     print("on_circle_press")
+    # def on_circle_release(self):
+    #     print("on_circle_release")
 
-    def on_triangle_release(self):
-        print("on_triangle_release")
+    # def on_square_press(self):
+    #     print("on_square_press")
+    # def on_square_release(self):
+    #     print("on_square_release")
 
-    def on_circle_press(self):
-        print("on_circle_press")
-
-    def on_circle_release(self):
-        print("on_circle_release")
-
-    def on_square_press(self):
-        print("on_square_press")
-
-    def on_square_release(self):
-        print("on_square_release")
-
+   ################# TURN SIGNAL FUNCTIONS #################  
     def on_L1_press(self):
-        print("on_L1_press")
+        print("LEFT TURN SIGNAL ENGAGED")
     def on_L1_release(self):
-        print("on_L1_release")
+        print("LEFT TURN SIGNAL DISENGAGED")
+    def on_R1_press(self):
+        print("RIGHT TURN SIGNAL ENGAGED")
+    def on_R1_release(self):
+        print("RIGHT TURN SIGNAL DISENGAGED")
+   #######################################$#################  
 
    ################# BRAKE FUNCTIONS #################  
     def on_L2_press(self, value):
@@ -47,11 +49,6 @@ class MyController(Controller):
     def on_L2_release(self):
         print("BRAKE DISENGAGED")
     ##################################################
-    
-    def on_R1_press(self):
-        print("on_R1_press")
-    def on_R1_release(self):
-        print("on_R1_release")
 
    ############## THROTTLE FUNCTIONS #################  
     def on_R2_press(self, value):
@@ -60,101 +57,82 @@ class MyController(Controller):
         print("THROTTLE DISENGAGED")
     ##################################################
 
-    def on_up_arrow_press(self):
-        print("on_up_arrow_press")
+    # def on_up_arrow_press(self):
+    #     print("on_up_arrow_press")
+    # def on_up_down_arrow_release(self):
+    #     print("on_up_down_arrow_release")
+    # def on_down_arrow_press(self):
+    #     print("on_down_arrow_press")
+    # def on_left_arrow_press(self):
+    #     print("on_left_arrow_press")
+    # def on_left_right_arrow_release(self):
+    #     print("on_left_right_arrow_release")
+    # def on_right_arrow_press(self):
+    #     print("on_right_arrow_press")
 
-    def on_up_down_arrow_release(self):
-        print("on_up_down_arrow_release")
-
-    def on_down_arrow_press(self):
-        print("on_down_arrow_press")
-
-    def on_left_arrow_press(self):
-        print("on_left_arrow_press")
-
-    def on_left_right_arrow_release(self):
-        print("on_left_right_arrow_release")
-
-    def on_right_arrow_press(self):
-        print("on_right_arrow_press")
-
-    def on_L3_up(self, value):
-        print("on_L3_up: {}".format(value))
-
-    def on_L3_down(self, value):
-        print("on_L3_down: {}".format(value))
-
+   ############## STEERING WHEEL FUNCTIONS #################  
     def on_L3_left(self, value):
-        print("on_L3_left: {}".format(value))
-
+        print("STEER LEFT: {}%".format(mapRangePS4Value(value)))
     def on_L3_right(self, value):
-        print("on_L3_right: {}".format(value))
-
-    def on_L3_y_at_rest(self):
-        """L3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_L3_y_at_rest")
-
+        print("STEER RIGHT: {}%".format(mapRangePS4Value(value)))
     def on_L3_x_at_rest(self):
-        """L3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_L3_x_at_rest")
+        print("STEER REST")
 
-    def on_L3_press(self):
-        """L3 joystick is clicked. This event is only detected when connecting without ds4drv"""
-        print("on_L3_press")
+    # def on_L3_up(self, value):
+        # print("on_L3_up: {}".format(value))
+    # def on_L3_down(self, value):
+        # print("on_L3_down: {}".format(value))
+    # def on_L3_y_at_rest(self):
+    #     """L3 joystick is at rest after the joystick was moved and let go off"""
+    #     print("on_L3_y_at_rest")
+    # def on_L3_press(self):
+    #     """L3 joystick is clicked. This event is only detected when connecting without ds4drv"""
+    #     print("on_L3_press")
+    # def on_L3_release(self):
+    #     """L3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
+    #     print("on_L3_release")
+    ######################################################
 
-    def on_L3_release(self):
-        """L3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
-        print("on_L3_release")
+    # def on_R3_up(self, value):
+    #     print("on_R3_up: {}".format(value))
+    # def on_R3_down(self, value):
+    #     print("on_R3_down: {}".format(value))
+    # def on_R3_left(self, value):
+    #     print("on_R3_left: {}".format(value))
+    # def on_R3_right(self, value):
+    #     print("on_R3_right: {}".format(value))
+    # def on_R3_y_at_rest(self):
+    #     """R3 joystick is at rest after the joystick was moved and let go off"""
+    #     print("on_R3_y_at_rest")
+    # def on_R3_x_at_rest(self):
+    #     """R3 joystick is at rest after the joystick was moved and let go off"""
+    #     print("on_R3_x_at_rest")
+    # def on_R3_press(self):
+    #     """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
+    #     print("on_R3_press")
+    # def on_R3_release(self):
+    #     """R3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
+    #     print("on_R3_release")
 
-    def on_R3_up(self, value):
-        print("on_R3_up: {}".format(value))
-
-    def on_R3_down(self, value):
-        print("on_R3_down: {}".format(value))
-
-    def on_R3_left(self, value):
-        print("on_R3_left: {}".format(value))
-
-    def on_R3_right(self, value):
-        print("on_R3_right: {}".format(value))
-
-    def on_R3_y_at_rest(self):
-        """R3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_R3_y_at_rest")
-
-    def on_R3_x_at_rest(self):
-        """R3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_R3_x_at_rest")
-
-    def on_R3_press(self):
-        """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
-        print("on_R3_press")
-
-    def on_R3_release(self):
-        """R3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
-        print("on_R3_release")
-
+   ################# GEAR SHIFT MENU #################  
     def on_options_press(self):
-        print("on_options_press")
-
+        print("GEAR SHIFT MENU OPENED")
     def on_options_release(self):
-        print("on_options_release")
+        print("GEAR SHIFT MENU CLOSED")
+   ###################################################  
 
-    def on_share_press(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_share_press")
-
-    def on_share_release(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_share_release")
-
-    def on_playstation_button_press(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_playstation_button_press")
-
-    def on_playstation_button_release(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_playstation_button_release")
+    # def on_share_press(self):
+    #     """this event is only detected when connecting without ds4drv"""
+    #     print("on_share_press")
+    # def on_share_release(self):
+    #     """this event is only detected when connecting without ds4drv"""
+    #     print("on_share_release")
+    # def on_playstation_button_press(self):
+    #     """this event is only detected when connecting without ds4drv"""
+    #     print("on_playstation_button_press")
+    # def on_playstation_button_release(self):
+    #     """this event is only detected when connecting without ds4drv"""
+    #     print("on_playstation_button_release")
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
