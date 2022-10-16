088ecfd15bd2cf7801acfebc167525edc9d95934
Glazed_Darnut <dartonaili@gmail.com>
2022-10-16 01:59:04 -0400

    last changes before merge

--- video_streaming/webcam_capture.py
+++ video_streaming/webcam_capture.py
@@ -1,7 +1,7 @@
 from subprocess import *
 
 
-
+# TODO: Add in webcam from opencv
 class ProcessOutput(object):
     def __init__(self, camera):
         print('Spawning background conversion process')
@@ -28,3 +28,20 @@ class ProcessOutput(object):
         print('Waiting for background conversion process to exit')
         self.converter.stdin.close()
         self.converter.wait()
+
+class BroadcastThread(Thread):
+    def __init__(self, converter, websocket_server):
+        super(BroadcastThread, self).__init__()
+        self.converter = converter
+        self.websocket_server = websocket_server
+
+    def run(self):
+        try:
+            while True:
+                buf = self.converter.stdout.read1(32768)
+                if buf:
+                    self.websocket_server.manager.broadcast(buf, binary=True)
+                elif self.converter.poll() is not None:
+                    break
+        finally:
+            self.converter.stdout.close()
